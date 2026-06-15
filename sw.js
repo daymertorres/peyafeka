5a46c9e11504bbe0a80c552b88d748c16f4673f3
daymertorres <114095646+daymertorres@users.noreply.github.com>
2026-06-14 19:54:19 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('yape-store').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './yape.html',
+      './YAPE.svg',
+      './yape.jpg'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
